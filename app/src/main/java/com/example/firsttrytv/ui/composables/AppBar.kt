35ea1d08bec998df90e2ae538e9641aa5a1a3675
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-02-15 17:26:55 +0200

    Update AppBar profile Image

--- app/src/main/java/com/example/firsttrytv/ui/composables/AppBar.kt
+++ app/src/main/java/com/example/firsttrytv/ui/composables/AppBar.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
@@ -23,7 +26,13 @@ fun AppBar() {
             .padding(vertical = MaterialTheme.dimens.size32),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Image(painter = painterResource(R.drawable.ic_profile), contentDescription = null)
+        Image(
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(MaterialTheme.dimens.size32),
+            painter = painterResource(R.drawable.profile_image),
+            contentDescription = null
+        )
         Spacer(modifier = Modifier.width(MaterialTheme.dimens.size20))
         AppBarOptions(text = "Home", isSelected = true)
         AppBarOptions(text = "Categories", isSelected = false)
